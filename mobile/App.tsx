c51b3d26db3af9cd2fb54f46d9d4903bbe2ba77b
Jeferson Oliveira <jeferson.rjrn@gmail.com>
2020-08-06 20:12:31 -0300

    landing page style

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo'; // a splash art de quando o app ta carregando
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 export default function App() {
-  return (
-    <>
-    <Landing />
-    <StatusBar style="auto" />
-    </>
-  );
+
+  let [fontsLoaded] = useFonts({ // fontsLoaded=true quando terminar de carregar as fontes
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) { // Enquanto as fontes não carregam, mostro a splash art
+    return <AppLoading />;
+  } else {
+
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
---
+++ mobile/src/@types/index.d.ts
@@ -0,0 +1,5 @@
+// Esse aquivo é somente lido pelo typescript e não pela aplicação
+
+
+// Todo arquivo que termina com png eu consigo importar
+declare module '*.png';
