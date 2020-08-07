835145d9509e2c5214db747e359e3c67fa5c477e
Jeferson Oliveira <jeferson.rjrn@gmail.com>
2020-08-06 22:54:44 -0300

    add style to pages teacherlist and favorites

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import styles from './styles';
 import PageHeader from '../../components/PageHeader';
 
-function Favorites() {
+import styles from './styles';
+
+function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Meus proffys favoritos" />
+      <PageHeader title="Proffys disponíveis"/>
     </View>
   );
 }
 
-export default Favorites;
+export default TeacherList;
